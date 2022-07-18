05c57959f4687c0039b324e12ddb879cfa8f5a71
RenukaSrivalli <107935355+RenukaSrivalli@users.noreply.github.com>
2022-07-18 21:14:38 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,14 @@
+class Renuka
+{
+public void add(int a,int b)
+{
+System.out.println("Addition of two numbers "+(a+b));
+}
+class Addition
+{
+public static void main(String args[])
+{
+Renuka obj=new Renuka();
+obj.add(5,5);
+}
+}
